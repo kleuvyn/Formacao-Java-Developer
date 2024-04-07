1a7e4f8585377bf2a4b9719642e65f4145864984
kleuvyn <kleuvyn@hotmail.com>
2024-04-06 21:26:55 -0300

    aula 2

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,15 @@
 public class MinhaClasse {
+
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+     
+        String primeiroNome  = "Beatryz";
+        String segundoNome = "Kleuvyn";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do métado " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
